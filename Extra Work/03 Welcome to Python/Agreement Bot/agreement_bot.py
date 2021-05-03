875a57c8f668c08f733d6d05cbb94f28e6ad50a1
Kim Oba <kimobachi@gmail.com>
2021-05-02 23:59:49 -0400

    added

---
+++ Extra Work/03 Welcome to Python/Agreement Bot/agreement_bot.py
@@ -0,0 +1,8 @@
+def main():
+    user_fav_animal = input("What is your favorite animal? ")
+    print("My favorite animal is also " + user_fav_animal + "!")
+    # print(f"My favorite animal is also {user_fav_animal}!")
+    # print("My favorite animal is also", user_fav_animal, "!") #  adds an extra space at the end
+
+if __name__ == "__main__":
+    main()
