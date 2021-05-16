345d044f75b66d6a33e27091481ab1a38fef2e5a
Kim Oba <kimobachi@gmail.com>
2021-05-15 23:13:36 -0400

    Get name solution

---
+++ Extra Work/03 Parameters and Returning/Return/Get name/get_name.py
@@ -0,0 +1,10 @@
+def get_name():
+    # delete the pass statement below and write your code here!
+    return "Karel"
+
+def main():
+    name = get_name() # get_name() will return a string which we store to the 'name' variable here
+    print("Howdy", name, "! 🤠")
+
+if __name__ == "__main__":
+    main()
